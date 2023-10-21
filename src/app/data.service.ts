c84095a2531b3c6def19d76eb6189c521eeeea56
ebharathi <elayabharathi10102002@gmail.com>
2023-10-21 15:40:54 +0530

    added backend

---
+++ src/app/data.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders, HttpParams } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  
+  constructor(private http:HttpClient) { }
+  private url:string="http://localhost:9000";
+  signup(username:string,password:string)
+  {
+      let signupUrl=`${this.url}/signup`;
+      const data={
+        username:username,
+        password:password
+      }
+      const httpOptions={
+        headers:new HttpHeaders({
+          'content-type': 'application/json'
+        })
+      }
+      return this.http.post(signupUrl,data,httpOptions);
+  }
+}
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { DataService } from '../data.service';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-
+    constructor(private dataService:DataService){}
+    public error:any="";
+    Onsubmit(name:string,pass:string)
+    { 
+      //  event.preventDefault();
+       console.log("[+]SIGNUP CALLED");
+       if(name==""||pass=="")
+       {
+         this.error="Please enter name & password";
+         return
+       }
+       this.dataService.signup(name,pass).subscribe((response:any)=>{
+              console.log("RESPONSE FROM BACKEND FOR SIGN UP:",response);
+       })
+    }
 }
